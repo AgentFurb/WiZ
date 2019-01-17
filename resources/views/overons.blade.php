df455f4fcdc487f34cc4b5268d0888aac9024d3b
Ferdy Hommeles <fhommeles@kuijpers.com>
2019-01-17 15:16:07 +0100

    Overons images

--- resources/views/overons.blade.php
+++ resources/views/overons.blade.php
@@ -6,15 +6,17 @@
         <div class="row">
             <div class="col"></div>
                 <div class="OverOnsText ">
-                    <p><h1 id="overonsheader">Over ons</h1>
-                    Welkom op WiZ Kuijpers, <br>
-                    Wij zijn Daan Swinkels & Ferdy Hommeles, wij doen de opleiding Applicatatie- en Mediaontwikkeling op het
-                    Summa College in Eindhoven en het ROC Ter AA in Helmond.<br> 
-                    Als stage opdracht bij Kuijpers Buisiness Partners in Helmond
-                    hebben wij de WiZ applicatie gemaakt in samenwerking met Kuijpers. WiZ staat voor Weggooien is Zonde, de reden waarom
-                    we deze website hebben gebouwd is omdat Kuijpers een beter inzicht wil krijgen welke producten er zijn overgebleven na het 
-                    afronden van een project.
-                    <br> </p>
+                    <p>
+                        <h1 id="overonsheader">Over ons</h1>
+                        Welkom op WiZ Kuijpers, <br>
+                        Wij zijn Daan Swinkels & Ferdy Hommeles, wij doen de opleiding Applicatatie- en Mediaontwikkeling op het
+                        Summa College in Eindhoven en het ROC Ter AA in Helmond.<br> 
+                        Als stage opdracht bij Kuijpers Buisiness Partners in Helmond
+                        hebben wij de WiZ applicatie gemaakt in samenwerking met Kuijpers. WiZ staat voor Weggooien is Zonde, de reden waarom
+                        we deze website hebben gebouwd is omdat Kuijpers een beter inzicht wil krijgen welke producten er zijn overgebleven na het 
+                        afronden van een project.
+                        <br> 
+                    </p>
                 </div>
             <div class="col"></div>
         </div>        
@@ -23,19 +25,13 @@
     <div class="container-fluid">
         <div class="row ">
             <div class="col overonscols">
-                <div class="overonsimages">
-                    <img src="img/20190115_134053.jpg" class="rotateimgoverons" height="300px" width="300px">
-                </div>
+                <img src="img/20190115_134053.jpg" class="rotateimgoverons overonsimages" height="300px" width="300px">
             </div>
             <div class="col overonscols">
-                <div class="overonsimages">
-                    <img src="img/20190115_133321.jpg" height="300px" width="300px">
-                </div>
+                <img src="img/20190115_133321.jpg" class="overonsimages" height="300px" width="300px">
             </div>
             <div class="col overonscols">
-                <div class="overonsimages">
-                    <img src="img/20190115_134546.jpg" height="300px" width="300px">
-                </div>
+                <img src="img/20190115_134546.jpg" class="overonsimages" height="300px" width="300px">
             </div>
         </div>
         <div class="row">
