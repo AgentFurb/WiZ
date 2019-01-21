6669c7677af555d74952612e0bccc1cd323af2fb
Ferdy Hommeles <fhommeles@kuijpers.com>
2019-01-21 08:49:07 +0100

    Spelfouten Over Ons opgelost

--- resources/views/overons.blade.php
+++ resources/views/overons.blade.php
@@ -9,9 +9,9 @@
                     <p>
                         <h1 id="overonsheader">Over ons</h1>
                         Welkom op WiZ Kuijpers, <br>
-                        Wij zijn Daan Swinkels & Ferdy Hommeles, wij doen de opleiding Applicatatie- en Mediaontwikkeling op het
+                        Wij zijn Daan Swinkels & Ferdy Hommeles, wij doen de opleiding Applicatie- en Mediaontwikkeling op het
                         Summa College in Eindhoven en het ROC Ter AA in Helmond.<br> 
-                        Als stage opdracht bij Kuijpers Buisiness Partners in Helmond
+                        Als stage opdracht bij Kuijpers Business Partners in Helmond
                         hebben wij de WiZ applicatie gemaakt in samenwerking met Kuijpers. WiZ staat voor Weggooien is Zonde, de reden waarom
                         we deze website hebben gebouwd is omdat Kuijpers een beter inzicht wil krijgen welke producten er zijn overgebleven na het 
                         afronden van een project.
