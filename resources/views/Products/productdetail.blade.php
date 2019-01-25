ed5c620342b780cc881e39eebb971c764a9854ea
Daan Swinkels <dswinkels@kuijpers.com>
2019-01-25 15:12:59 +0100

    lil fix modal

--- resources/views/Products/productdetail.blade.php
+++ resources/views/Products/productdetail.blade.php
@@ -111,7 +111,7 @@
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
-                <div class="modal-content detailmodal">
+                <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">{{ $productdetail[0]->productomschrijving }} specificaties</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
