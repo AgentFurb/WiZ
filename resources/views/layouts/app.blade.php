c7a6620131cdf8649d3bcd196ab1f0ca2e65df0e
Ferdy Hommeles <fhommeles@kuijpers.com>
2019-01-29 10:19:39 +0100

    Google Lighthouse test Loginpage PWA, SEO, BestPractices & Accessibility

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <meta name=viewport content="width=device-width, initial-scale=1"/>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="{{ asset('fontawesome/css/all.css') }}" />
-    <!-- CSRF Token -->
+    <title>WiZ Kuijpers - Inloggen</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>WiZ</title>
+    <meta name="description" content="WiZ Kuijpers - Weggooien is Zonde, een overzicht van alle overgbleven producten van Kuijpers."/>
+    <meta name="author" content="Daan Swinkels, Ferdy Hommeles">
+    <meta name="keywords" content="Kuijpers,WiZ, Weggooien is Zonde">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -15,6 +15,8 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}" />
     <link rel="shortcut icon" type="image/png" href="{{ asset('img/wizicon.png')}}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}" />
+
+    @laravelPWA
 
 </head>
 <body>
